15fa2726ee4518245178f1ad88c008ea27d5d6f2
ADi <allandelgadoc@gmail.com>
2021-06-16 22:17:47 -0600

    Add project files.

---
+++ MyStuff/MyStuff/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using MyStuff.ViewModels;
+using MyStuff.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace MyStuff
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
